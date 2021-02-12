3bc6ce9588eec86e34482dfcacc7e6868d9dc779
MAbdurahman <mdbdrrhm@gmail.com>
2021-02-12 03:13:16 -0600

    SideMenu: add useStyles and makeStyles

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import SideMenu from '../components/side-menu/SideMenu';
 
 
 export default function App() {
   return (
     <div>
-      <h1>Material UI Tutorial</h1>
+      <SideMenu />
     </div>
   );
 };
--- src/components/side-menu/SideMenu.jsx
+++ src/components/side-menu/SideMenu.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+   sideMenu: {
+      position: 'absolute',
+      left: '0',
+      width: '320px',
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      backgroundColor: '#423E37',
+      color: '#EDEBD7',
+   }
+})
 
 export default function SideMenu() {
+   const classes = useStyles();
    return (
-      <div>
+      <div className={classes.sideMenu}>
          <h1>SideMenu</h1>
       </div>
    );
